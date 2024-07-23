aa627043873c7c2fbe6e1569778379e1db976200
Anton Patsev <patsev.anton@gmail.com>
2024-07-23 11:58:31 +0600

    fix tflint required_providers

--- versions.tf
+++ versions.tf
@@ -1,7 +1,12 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.72.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
     }
   }
   required_version = ">= 1.3, < 1.6"
